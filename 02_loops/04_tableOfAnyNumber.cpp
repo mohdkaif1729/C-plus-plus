cd8d4b2f91bb146b04671f82509b64c79540e40f
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-07-14 16:19:00 +0530

    table of n number

---
+++ 02_loops/04_tableOfAnyNumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num;
+  cout << "Enter a number: ";
+  cin >> num;
+
+  for (int i = 1; i <= 10; i++)
+  {
+    cout << num * i << endl;
+  }
+
+  return 0;
+}
