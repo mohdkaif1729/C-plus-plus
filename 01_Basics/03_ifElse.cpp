c4a89cae4d64558ff0ce814a28846aa0b0d2cf38
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-07-14 14:21:23 +0530

    if else is done

---
+++ 01_Basics/03_ifElse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int num = 3;
+
+  if (num > 0)
+  {
+    cout << "Number is positive";
+  }
+  else
+  {
+    cout << "Number is negative";
+  }
+
+  return 0;
+}
